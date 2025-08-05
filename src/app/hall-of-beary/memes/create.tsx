0de632804f6d9808aa89dc0595b8a2588140cbd6
BagasBikang <bagasadit78@gmail.com>
2025-08-06 03:49:08 +0700

    fix: upload memes

--- src/app/hall-of-beary/memes/create.tsx
+++ src/app/hall-of-beary/memes/create.tsx
@@ -22,7 +22,7 @@ export default function UploadMeme() {
         <DialogContent className="w-sm md:w-xl p-6">
             <DialogTitle className="text-2xl font-bold mb-4">Upload Meme</DialogTitle>
             <div>
-                <form action="http://api.phaserxrp.xyz/api/memes/create" method="post" encType="multipart/form-data">
+                <form action="https://api.phaserxrp.xyz/api/memes/create" method="post" encType="multipart/form-data">
                     <label className="block text-xs md:text-sm font-medium m-0 p-0 text-red-500 mb-2">*PNG, JPG, JPEG, WEBP (MAX. 1MB).</label>
                     <input type="file" className="w-75 md:w-full border border-black p-2 rounded-lg bg-black/20 hover:bg-black/30" name="image" id="image" required/>
                     <button type="submit" className="text-white bg-black hover:bg-gray-800 p-2 mt-3 rounded-lg">Submit</button>
